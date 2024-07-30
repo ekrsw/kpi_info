4c9cd0349e9008ab27505f031347d8aa51991d4a
ekrsw <eisuke.koresawa@gmail.com>
2024-07-30 10:41:04 +0900

    while True

--- main.py
+++ main.py
@@ -3,15 +3,16 @@ import time
 from app.controllers.summary import Summary
 import datetime
 
-start = datetime.datetime.now()
-summary = Summary('TVS')
-summary.add_kpi()
-end = datetime.datetime.now()
+while True:
+    start = datetime.datetime.now()
+    summary = Summary('TVS')
+    summary.add_kpi()
+    end = datetime.datetime.now()
 
-# 処理時間と待機時間で60秒になるように調整
-print('待機中...')
-remaining_time = datetime.timedelta(seconds=60) - (end - start)
-if remaining_time.total_seconds() > 3.0:
-    time.sleep(remaining_time.total_seconds())
-else:
-    time.sleep(3.0)
+    # 処理時間と待機時間で60秒になるように調整
+    print('待機中...')
+    remaining_time = datetime.timedelta(seconds=60) - (end - start)
+    if remaining_time.total_seconds() > 3.0:
+        time.sleep(remaining_time.total_seconds())
+    else:
+        time.sleep(3.0)
